bebd24fda698e31d8cc9feff27e1e08ee3f78e95
furuyama0912 <68705777+furuyama0912@users.noreply.github.com>
2023-06-15 05:06:34 +0900

    課題

--- 2-1/src/study/Main.java
+++ 2-1/src/study/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package study;
 
 /**
  *
@@ -8,26 +8,26 @@ package sample;
  * 指定された値と変数名を守って記述してください。
  *
  */
-public class  Hoge {
+public class Main {
 
     public static void main(String[] args) {
 
       //① firstNameとlastNameという名前の変数を定義し、
       //   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
-    	String firstName = "Kana";
-    	String lastName =  "Haraguchi";
+    	データ型名 firstName = 値;
+    	データ型名 lastName = 値;
 
-
-        //getNat.println();
-    	System.out.println(getName(firstName,lastName));
-    	System.out.println();
-    	
+        //getName関数の呼び出しと出力
+        System.out.println(getName(firstName, lastName));
+        System.out.println();
+        
 
         int[] arr = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
-        for (int i = 0; i < arr.length; i++) {
+        for (初期化式; 条件式; 変化式){
+        	  // 繰り返しの中で実行される処理
         	isOdd(arr[i]);
         }
 
@@ -40,8 +40,12 @@ public class  Hoge {
     //  修飾子：public
     //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
     //  連結した値を返り値とする変数を定義しなさい。
-    public static String getName(String firstName, String lastName);
-     return String firstName, string lastName;
+    public static 〇〇 〇〇() {
+        return ○ + ○;
+    }
+
+
+
 
     //奇数の判定を行う。
     public static void isOdd(int num) {
