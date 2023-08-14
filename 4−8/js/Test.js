69db5b4c6ddfb2d14d6b7e81f48c72c265c79cc4
furuyama0912 <68705777+furuyama0912@users.noreply.github.com>
2023-08-14 17:06:00 +0900

    課題の確認、よろしくな！

--- 4−8/js/Test.js
+++ 4−8/js/Test.js
@@ -1,5 +1,24 @@
-$(".slide-items").slick({
-    autoplay: true, // 自動再生
-    autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
-    infinite: true, // 無限スライド
+$('.slider').slick({
+    autoplay: true,       //自動再生
+    autoplaySpeed: 2000,  //自動再生のスピード
+    speed: 800,           //スライドするスピード
+    dots: true,           //スライド下のドット
+    arrows: true,         //左右の矢印
+    infinite: true,       //永久にループさせる
+});
+
+
+
+const modal = $('#modal-container');
+const img = modal.find('img');
+
+$('img.popup').each(function(index) {
+    $(this).click(function() {
+        img.attr('src', $(this).attr('src'));
+        modal.show();
+    })
+});
+
+modal.click(function() {
+    $(this).hide();
 });
